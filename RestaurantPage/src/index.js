50d5e659b64030bcf39930ea35b33ee115c43c30
Bbyz <bbyzdev@gmail.com>
2024-02-11 15:28:50 +0100

    Added the tab switching logic

--- RestaurantPage/src/index.js
+++ RestaurantPage/src/index.js
@@ -3,4 +3,24 @@ import LoadMenu from "./menu";
 import LoadContact from "./contact";
 
 const content = document.getElementById('content');
-content.append(LoadContact());
+let tabs = Array.from(document.querySelectorAll('button'));
+
+tabs[0].addEventListener('click', () => 
+{
+    content.innerHTML = "";
+    content.append(LoadHome());
+});
+
+tabs[1].addEventListener('click', () => 
+{
+    content.innerHTML = "";
+    content.append(LoadMenu());
+});
+
+tabs[2].addEventListener('click', () => 
+{
+    content.innerHTML = "";
+    content.append(LoadContact());
+});
+
+content.append(LoadHome());
